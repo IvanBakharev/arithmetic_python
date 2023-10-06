d29e9509f2c9776384e88b03bd55a092386c158d
Akuma <ragna.blood82@gmail.com>
2023-10-06 12:00:04 +0300

    mission two complete

--- arithmetic.py
+++ arithmetic.py
@@ -1,26 +1,4 @@
-#  int generalNumber = 98657;
-
- # int tensOfThousands = generalNumber / 10000; // Десятки тысяч
-# int thousand = (generalNumber - tensOfThousands * 10000) / 1000; // Тысячи
-# int hundred = (generalNumber - tensOfThousands * 10000 - thousand * 1000) / 100; // Сотни
-# int ten = (generalNumber - tensOfThousands * 10000 - thousand * 1000 - hundred * 100) / 10; // Десятки
-# int unit = generalNumber - tensOfThousands * 10000 - thousand * 1000 - hundred * 100 - ten * 10; // Единицы
-
-# Console.WriteLine(tensOfThousands + " " + thousand + " " + hundred + " " + ten + " " + unit);
-
-# // Возведение десятков в степень равную кол-ву единиц
- # int exponentiationOfNumber = (int)Math.Pow(ten, unit);
-
- # // Умножение получившегося числа на кол-во сотен
- #int multiplication = exponentiationOfNumber * hundred;
-
- # // Деление получившегося числа на разность кол-ва десятков тысяч и кол-ва тысяч
- # double division = multiplication / (tensOfThousands - thousand);
-
- # Console.WriteLine("1. Возведение десятков в степень равную кол-ву единиц, число А: " + exponentiationOfNumber +
- #    "\n2. Умножение получившегося числа A на кол-во сотен, число В: " + multiplication +
-#   "\n3. Деление получившегося числа В на разность кол-ва десятков тысяч и кол-ва тысяч: " + division);
-
+#
       #  Задание первое
 
 sideA = float(input("Введите сторону сторону треугольника А: "))
@@ -53,5 +31,15 @@ print((side_A * 2) + (side_B * 2))
 print("Площадь прямоугольника равна: ", sep="=")
 print(side_A * side_B)
 
+
+number = int(input("Enter number:"))
+
+units = number % 10
+dozens = number % 100 // 10
+hundreads = number % 1000 // 100
+thousands = number % 10000 // 1000
+doz_of_thous = number // 10000
+
+print((dozens ** units) * hundreads / (doz_of_thous - thousands))
 
 
